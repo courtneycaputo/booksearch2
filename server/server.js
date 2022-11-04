c7c662c76e2e22f5dd0469f9d457c1b4df9db4a1
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-11-04 09:52:16 -0700

    install dependencies and add apollo to server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+// no longer need routes
+// const routes = require('./routes');
+
+// add apollo
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// integrate apollo server with express app as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +30,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// no longer using routes
+// app.use(routes);
+
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
