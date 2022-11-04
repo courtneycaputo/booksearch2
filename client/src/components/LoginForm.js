9cdef11fafa64c7489a8e34138fcb12018ce91ff
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-11-04 13:36:11 -0700

    import mutations

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,8 +2,12 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+
+// import mutations
+import { useMutation } from '@apollo/react-hooks';
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
